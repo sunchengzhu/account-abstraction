d8ff01f217cab5ec591c251abf9b342a2fd4250d
magicalne <magicalne@gmail.com>
2023-01-11 10:23:01 +0800

    chore: add testnet config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -76,6 +76,11 @@ const config: HardhatUserConfig = {
       accounts: [`0x${TEST_PK1}`, `0x${TEST_PK2}`, `${PRIVATE_KEY0}`, `${PRIVATE_KEY1}`],
       chainId: 202206
     },
+    gw_testnet_v1: {
+      url: `https://v1.testnet.godwoken.io/rpc/instant-finality-hack`,
+      accounts: [`0x${TEST_PK1}`, `0x${TEST_PK2}`, `${PRIVATE_KEY0}`, `${PRIVATE_KEY1}`],
+      chainId: 71401,
+    },
     dev: { url: 'http://localhost:8545' },
     // github action starts localgeth service, for gas calculations
     localgeth: { url: 'http://localgeth:8545' },
